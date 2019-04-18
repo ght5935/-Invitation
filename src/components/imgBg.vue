c39ce2a54dd8ccad619b92c6fda68d5019a0a152
gaohuatao <ght5935@163.com>
2019-04-18 17:54:17 +0800

    first commit

---
+++ src/components/imgBg.vue
@@ -0,0 +1,22 @@
+<template>
+    <image class="imgBg" src="/static/images/bg.jpg"/>
+</template>
+
+<script>
+export default {
+  name: 'imgBg',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.imgBg
+    position absolute
+    width 100%
+    height 100%
+    z-index -1
+</style>
---
+++ src/pages/photo/index.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="photo">
+        <Himg></Himg>
+        <h-swiper :list="list" :isGif="isGif"></h-swiper>
+    </div>
+</template>
+
+<script>
+import HSwiper from 'components/swiper'
+import Himg from "components/imgBg";
+export default {
+  name: 'Photo',
+  components: {
+    HSwiper,
+    Himg
+  },
+  onShow () {
+    const that = this
+    that.isGif = !that.isGif
+    that.getList()
+  },
+
+  data () {
+    return {
+      list: [],
+      isGif: false
+    }
+  }, 
+  methods: {
+    getList () {
+      const that = this
+      const db = wx.cloud.database()
+      const banner = db.collection('photo')
+      banner.get().then(res => {
+        that.list = res.data
+      })
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.photo
+    height 100%
+</style>
+
